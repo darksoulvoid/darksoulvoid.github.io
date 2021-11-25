424ea2b0b40a2353694e3adf894b0ecf9bfba316
darksoulvoid <93506478+darksoulvoid@users.noreply.github.com>
2021-11-25 15:51:21 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+VANTA.FOG({
+  el: "#intro",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  highlightColor: 0x38aad2,
+  midtoneColor: 0x3eaf84,
+  lowlightColor: 0xfff00,
+  baseColor: 0x51764,
+  speed: 1.10,
+  zoom: 1.30
+})
